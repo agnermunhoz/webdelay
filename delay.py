2cbc6f5b370ec53a4d76087202a37a7927eefc3e
Agner Munhoz <agner.munhoz@gmail.com>
2022-06-13 15:24:32 -0300

    Update delay.py

--- delay.py
+++ delay.py
@@ -1,13 +1,12 @@
-# Python 3 server example
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 
-hostName = "localhost"
+hostName = "0.0.0.0"
 serverPort = 8080
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        #time.sleep(60)
+        time.sleep(60)
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
@@ -18,11 +17,14 @@ class MyServer(BaseHTTPRequestHandler):
         self.wfile.write(bytes("</body></html>", "utf-8"))
 
 if __name__ == "__main__":
-    webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
+    webServer = HTTPServer((hostName, serverPort), MyServer)
 
+    print("Server started http://%s:%s" % (hostName, serverPort))
     try:
+        print("Server started http://%s:%s" % (hostName, serverPort))
         webServer.serve_forever()
+        print("Server started http://%s:%s" % (hostName, serverPort))
     except KeyboardInterrupt:
         pass
 
