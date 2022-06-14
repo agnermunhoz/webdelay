327d8d159705d3c991b947bc989f6fa2f30f5dc2
Agner Munhoz <agner.munhoz@gmail.com>
2022-06-14 09:52:08 -0300

    Update delay.py

--- delay.py
+++ delay.py
@@ -6,15 +6,17 @@ serverPort = 8080
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        time.sleep(700)
+        time.sleep(850)
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>Delay</head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<!DOCTYPE html>", "utf-8"))
+        self.wfile.write(bytes("<html>", "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>Web server with delay.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+        self.wfile.write(bytes("<h1>Delay</h1>", "utf-8"))
+        self.wfile.write(bytes("<p>850s delay</p>", "utf-8"))
+        self.wfile.write(bytes("</body>", "utf-8"))
+        self.wfile.write(bytes("</html>", "utf-8"))
 
 if __name__ == "__main__":
     print("Server started http://%s:%s" % (hostName, serverPort))
